c012cf7a6203254828469167cc2561f4fbd3bcdd
Sotom98 <48165847+Sotom98@users.noreply.github.com>
2019-05-15 00:15:25 -0600

    20190515

---
+++ DetalleFactura/Fomulario1/CSQL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace Fomulario1 {
+    class CSQL {
+        SqlConnection cnx;
+        SqlCommand cmd;
+        SqlDataAdapter dap;
+        DataTable dat;
+
+        public CSQL() {
+            string aviso;
+
+            try {
+                cnx = new SqlConnection("Data Source=.;Initial Catalog=DetalleFactura;Integrated Security=True");
+                cnx.Open();
+                aviso = "Conectado";
+            }
+            catch (Exception Ex) {
+                aviso = "Error" + Ex.ToString();
+            }
+        }
+
+        public void IngresarProductos(int a, string b, double c, int d) {
+            cmd = new SqlCommand("insert into Productos values(" + a + ",'" + b + "'," + c + "," + c + ")", cnx);
+            cmd.ExecuteNonQuery();
+        }
+
+        public void LlenarProductos(DataGridView dvg) {
+            dap = new SqlDataAdapter("select * from Productos", cnx);
+            dat = new DataTable();
+            dap.Fill(dat);
+            dvg.DataSource = dat;
+        }
+
+        public void LlenarClientes(DataGridView dvg) {
+            dap = new SqlDataAdapter("select * from Clientes", cnx);
+            dat = new DataTable();
+            dap.Fill(dat);
+            dvg.DataSource = dat;
+        }
+    }
+}
---
+++ DetalleFactura/Fomulario1/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fomulario1 {
+    public partial class Form1 : Form {
+        CSQL sql = new CSQL();
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // TODO: esta línea de código carga datos en la tabla 'detalleFacturaDataSet.Productos' Puede moverla o quitarla según sea necesario.
+            this.productosTableAdapter.Fill(this.detalleFacturaDataSet.Productos);
+            sql.LlenarProductos(dataGridView1);
+            sql.LlenarClientes(dataGridView2);
+
+        }
+
+        private void button2_Click(object sender, EventArgs e) {
+            int a = int.Parse(textBox1.Text);
+            string b = textBox2.Text;
+            double c = double.Parse(textBox3.Text);
+            int d = int.Parse(textBox4.Text);
+
+            sql.IngresarProductos(a, b, c, d);
+            sql.LlenarProductos(dataGridView1);
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+
+        }
+
+        private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e) {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e) {
+            int a = int.Parse(textBox5.Text);
+            string b = textBox7.Text;
+            double c = double.Parse(textBox3.Text);
+            int d = int.Parse(textBox4.Text);
+
+            sql.IngresarProductos(a, b, c, d);
+            sql.LlenarProductos(dataGridView1);
+
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+    }
+}
